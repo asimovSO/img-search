40a15211db5da6329360ef371c72f759c65063a5
asimovSO <svoleg2002@gmail.com>
2021-11-21 16:58:04 +0300

    done

---
+++ js/app.js
@@ -0,0 +1,31 @@
+
+
+const input = document.getElementById('search');
+const ul = document.querySelector('ul')
+
+
+input.addEventListener('keydown', e =>{
+    if(e.key == 'Enter'){
+        apiReq();
+    }
+})
+
+const url = `https://api.unsplash.com/search/photos?query=${input.value}&orientation=landscape&per_page=27&page=1&client_id=pV3bWqJz8xB5L1RZlKPRZn33yJCWHZ4W5WQSH9k_SFM`;
+
+const apiReq = () => {
+    ul.innerHTML = '';
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            loadImgs(data);
+        })
+
+
+function loadImgs(data){
+    for(i = 0; i < data.results.length; i++){
+        const elem = `<li><img src="${data.results[i].urls.raw}" width='390'></li>`;
+        ul.innerHTML += elem;
+    }
+}
+
+}
